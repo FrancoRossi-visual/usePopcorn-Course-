669313348e42ca32ec4ee735dcd32b3163dfae9b
Fran <francorossi.visual@gmail.com>
2023-09-29 21:23:36 -0300

    corrected

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import "./index.css";
-// import App from "./components/App";
-import StarRating from './components/StarRating';
+import './index.css';
+import App from './components/App';
+// import StarRating from './components/StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App> */}
-    <StarRating
-      maxRating={5}
-      messages={['Terrible', 'Bad', 'Good', 'Great', 'Excellent']}
-    />
-    <StarRating
-      maxRating={10}
-      size={24}
-      color='green'
-      className='test'
-      defaultRating={3}
-    />
+    <App />
   </React.StrictMode>
 );
